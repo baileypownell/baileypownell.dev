b84ea5dcdc0b915f77eafd22ea5000d885f1c5e3
Bailey Pownell <bailey.pownell@gmail.com>
2020-04-23 15:31:57 -0400

    4/23

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 const port = process.env.PORT || 3000;
-// serve static assets (aka javascript) from the dist folder in project root
+// serve static assets (javascript) from the dist folder in project root
 app.use(express.static(path.join(__dirname + '/dist')));
 
 app.get('/bpownell_resume.pdf', (req, res) => {
@@ -23,8 +23,26 @@ app.get('/bpownell_resume.pdf', (req, res) => {
 
 app.post('/contact', (request, response) => {
   const { name, email, message } = request.body;
-  console.log(name, email, message)
-  return response.json({success: true})
+
+  const transporter = nodemailer.createTransport(sgTransport({
+      service: 'SendGrid',
+      auth: {
+      api_user: `${process.env.SENDGRID_USERNAME}`,
+      api_key: `${process.env.SENDGRID_PASSWORD}`
+  }}));
+  const mailOptions = {
+    from: `${email}`,
+    to: `bailey.pownell@gmail.com`,
+    subject: `Portolio Email from ${name}`,
+    html: `${message}`
+  };
+  transporter.sendMail(mailOptions, (err, res) => {
+    if (err) {
+      return response.status(500).json({ success: false, message: 'there was an error sending the email', error: err.message, name: err.name})
+    } else {
+      return response.status(200).json('email sent');
+    }
+  })
 })
 
 app.get('*', (req, res) => {
