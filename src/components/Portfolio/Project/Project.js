59b112f0cb90cdbd0badb0ce4d0138f2246c5a44
Bailey Pownell <bailey.pownell@gmail.com>
2020-12-13 18:37:38 -0500

    updated virtual cookbook project component

--- src/components/Portfolio/Project/Project.js
+++ src/components/Portfolio/Project/Project.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import RWT from '../../../../dist/images/user-interface.png';
-import VC from '../../../../dist/images/VChomescreen.png';
+import VC from '../../../../dist/images/desktop_dashboard.png';
 import ISBA from '../../../../dist/images/ISBA.jpg';
 import BW from '../../../../dist/images/3bdhome.jpg';
 import MP from '../../../../dist/images/meal-picker.PNG';
--- src/components/VirtualCookbookProjectPage/VirtualCookbookProjectPage.js
+++ src/components/VirtualCookbookProjectPage/VirtualCookbookProjectPage.js
@@ -12,11 +12,12 @@ function VirtualCookbookProjectPage() {
       <Nav/>
       <div className="project-page">
         <h4>Virtual Cookbook SPA</h4>
-              <img className="project-photo" src="images/VChomescreen.png" ></img>
+              <img className="project-photo" src="images/desktop_dashboard.png" ></img>
               <p>Built with React, Node.js, Express, and PostgreSQL, this fullstack single page application involved a lot of software design concepts I hadn't used before. Although I would never do this for a truly production-level application, I decided to handle authentication myself. I used Nodemailer for sending emails when users want to change their password (for enhanced security), built the API from scratch, and used bcrypt.js for password hashing. I also added session-based authentication after reading a very convincing article against JWTs <a target="_blank" href="http://cryto.net/~joepie91/blog/2016/06/13/stop-using-jwt-for-sessions/">here</a>. This project came about not by following a tutorial but because I actually needed something like this in my life.
               </p>
-              <img className="project-photo" src="images/VCdashboard.png"></img>
-              <img className="project-photo" src="images/VCprofile.png"></img>
+              <img className="project-photo" src="images/desktop_dashboard_filtering.png"></img>
+              <img className="project-photo" src="images/recipe_desktop.png"></img>
+              <img className="project-photo mobile-view" src="images/settings_mobile.png"></img>
           <div className="button-holder">
               <a target="_blank" href="https://github.com/baileypownell/Virtual-Cookbook"><button>View Source Code</button></a>
               <a target="_blank" href="https://glacial-savannah-05736.herokuapp.com/"><button>Visit Project</button></a>
