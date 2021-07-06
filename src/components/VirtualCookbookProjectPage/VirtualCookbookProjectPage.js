2a0dbb6fdc7fb46ae66f7c4fa95a529922d6411b
Bailey Pownell <bailey.pownell@gmail.com>
2021-07-06 12:25:25 -0400

    added logos for deployed projects

--- src/components/VirtualCookbookProjectPage/VirtualCookbookProjectPage.js
+++ src/components/VirtualCookbookProjectPage/VirtualCookbookProjectPage.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import './ProjectPage.scss'
+import logo from './white-logo.png'
 
 function VirtualCookbookProjectPage() {
   
   return (
       <div className="project-page">
         <input style={{display: 'none'}} tabIndex="1"></input>
-        <h4>Recipe Stash</h4>
-              <img className="project-photo" src="images/desktop_dashboard.png" ></img>
-              <p>Built with React, Node.js, Express, MaterializeCSS, and PostgreSQL, this fullstack single page application involved a lot of software design concepts I hadn't used before. Although I would never do this for a truly production-level application, I decided to handle authentication myself. I used Nodemailer for sending emails when users want to change their password (for enhanced security), built the API from scratch, and used bcrypt.js for password hashing. I decided to use session-based authentication after reading a very convincing article against using JWTs for authentication <a target="_blank" href="http://cryto.net/~joepie91/blog/2016/06/13/stop-using-jwt-for-sessions/">here</a>. This project came about not by following a tutorial but because I actually needed something like this in my life.
-              </p>
-              <img className="project-photo" src="images/desktop_dashboard_filtering.png"></img>
-              <img className="project-photo" src="images/recipe_desktop.png"></img>
-              <p>I eventually added the ability to filter recipes by name and catgory. After that, I added functionality to "tag" recipes by features such as sugar-free, dairy-free, vegan, etc.</p>
-              <img className="project-photo mobile-view" src="images/settings_mobile.png"></img>
-              <p>This is a closed source project.</p>
-          <div className="button-holder">
-              <a target="_blank" href="https://recipe-stash-1.herokuapp.com/"><button>Visit Project</button></a>
-          </div>
+        <img className="logo" src={logo}></img>
+        <img className="project-photo" src="images/desktop_dashboard.png" ></img>
+        <p>
+          Built with React, Node.js, Express, MaterializeCSS, and PostgreSQL, Recipe Stash is a fullstack single page application involved a lot of software design concepts 
+          I hadn't used before. Although I would never do this for a truly production-level application, 
+          I decided to handle authentication myself. I used Nodemailer for sending emails when users want to change their password (for enhanced security), 
+          built the API from scratch, and used bcrypt.js for password hashing. I decided to use session-based authentication after 
+          reading a very convincing article against using JWTs for authentication purposes
+          <a target="_blank" href="http://cryto.net/~joepie91/blog/2016/06/13/stop-using-jwt-for-sessions/">here</a>. 
+          This project came about not by following a tutorial, but rather because I actually needed something like this.
+        </p>
+        <img className="project-photo" src="images/desktop_dashboard_filtering.png"></img>
+        <img className="project-photo" src="images/recipe_desktop.png"></img>
+        <p>
+          I eventually added the ability to filter recipes by name and catgory. 
+          After that, I added functionality to "tag" recipes by features such as sugar-free, dairy-free, vegan, etc.
+        </p>
+        <img className="project-photo mobile-view" src="images/settings_mobile.png"></img>
+        <p>This is a closed source project.</p>
+        <div className="button-holder">
+            <a target="_blank" href="https://recipe-stash-1.herokuapp.com/"><button>Visit Project</button></a>
+        </div>
       </div>
   )
 }
--- src/components/WeightTrackerProjectPage/WeightTrackerProjectPage.js
+++ src/components/WeightTrackerProjectPage/WeightTrackerProjectPage.js
@@ -1,21 +1,27 @@
 import React from 'react'
+import logo from './cover-dark.png'
 
 function WeightTrackerProjectPage() {
   
   return (
       <div className="project-page">
         <input style={{display: 'none'}} tabIndex="1"></input>
-        <h4>Happy Balance</h4>
-              <img className="project-photo" src="images/homescreen.PNG" ></img>
-              <p>I built this single-page web app not because I thought I could build a better program than the already-existing weight trackers, but because I wanted to use React and Firebase to build a full-stack CRUD application, complete with authentication. This project really pushed the boundaries of what I knew, and was the first project I deployed with heroku.
-              The most difficult part of the project was converting the dates stored in the back end into an appropriate format for the "date" column of the Recent Weight Logs box and comparing and sorting them for display to the user. I used Chart.js for the graph, which was a piece of cake.
-              One of the many reasons I chose React for this project is the possibility to use React Native to turn this into an actual mobile app.</p>
-              <img className="project-photo" src="images/user-interface.png"></img>
-              <img className="project-photo" src="images/rwt-mobile.png" className="mobile-image"></img>
-              <p>This is a closed source project.</p>
-          <div className="button-holder">
-              <a target="_blank" href="https://happy-balance.herokuapp.com/"><button>Visit Project</button></a>
-          </div>
+        <img className="logo" src={logo}/>
+        <img className="project-photo" src="images/homescreen.PNG" ></img>
+        <p>
+          I built this single-page web app not because I thought I could build a better program than the already-existing weight trackers, 
+          but because I wanted to use React and Firebase to build a full-stack CRUD application, complete with authentication. 
+          This project really pushed the boundaries of what I knew, and was the first project I deployed with heroku.
+          The most difficult part of the project was converting the dates stored in the back end into an appropriate format for the "date" column 
+          of the Recent Weight Logs box and comparing and sorting them for display to the user. I used Chart.js for the graph, which was a piece of cake.
+          One of the many reasons I chose React for this project is the possibility to use React Native to turn this into an actual mobile app.
+        </p>
+        <img className="project-photo" src="images/user-interface.png"></img>
+        <img className="project-photo" src="images/rwt-mobile.png" className="mobile-image"></img>
+        <p>This is a closed source project.</p>
+        <div className="button-holder">
+            <a target="_blank" href="https://happy-balance.herokuapp.com/"><button>Visit Project</button></a>
+        </div>
       </div>
   )
 }
