0285089baa99544e4e169f6e3c639f403a01ed67
Bailey Pownell <bailey.pownell@gmail.com>
2022-01-12 20:09:49 -0500

    button changes

--- src/components/LandingImage/LandingImage.js
+++ src/components/LandingImage/LandingImage.js
@@ -1,38 +1,20 @@
 import React, { useEffect } from 'react';
 
-import computer from '../../../dist/images/1633797727046.jpg';
+import me from '../../../dist/images/1633797727046.jpg';
 
 import './LandingImage.scss';
 
 class LandingImage extends React.Component {
-
-  fadeInElements = () => {
-    let fadedElements = document.querySelectorAll('.faded');
-    for (let i = 0; i < fadedElements.length; i++) {
-      let introPos = fadedElements[i].getBoundingClientRect().top;
-      let screenPos = window.innerHeight/1.2;
-      if (introPos < screenPos ) {
-        fadedElements[i].classList.add('fade-in');
-      }
-    }
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.fadeInElements()
-    }, 700);
-  }
-
   render() {
     return (
       <div className="landing-screen">
-        <img src={computer} />
+        <img src={me} />
         <div>
-        <div className="points faded">
+        <div className="points">
           <h1><span id="hello">Hello,</span><br/> my name is Bailey</h1>
           <h2>I am a software developer proficient in HTML, CSS, Javascript, Angular and React.</h2>
         </div>
-        <div className="skills faded">
+        <div className="buttons fade-in">
           <button className="swipe">Github <i className="fab fa-github"></i></button>
           <button className="swipe">LinkedIn <i className="fab fa-linkedin"></i></button>
           <button className="swipe">Resume <i className="far fa-file"></i></button>
