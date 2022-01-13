1a0bdf992381c1a8ef80b37b0dff3c9acd894819
Bailey Pownell <bailey.pownell@gmail.com>
2022-01-12 21:28:41 -0500

    prototype for svgs

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react';
 
 import './Navigation.scss';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
 
   return (
     <nav>
-      <a href="/home">Home</a>
+      <NavLink exact activeClassName="active" to='/'>Home</NavLink>
       <div>
-        <a href="/portfolio">Portfolio</a>
-        <a href="/contact">Contact</a>
+        <NavLink activeClassName="active" to='/portfolio'>Portfolio</NavLink>
+        <NavLink activeClassName="active" to='/contact'>Contact</NavLink>
       </div>
     </nav>
   )
